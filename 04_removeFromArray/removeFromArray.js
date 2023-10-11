1c67d86c2615dfb3a20b4b3cc43489fa1dc4048a
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-11 16:06:51 -0400

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...num) {
+  return array.filter((value) => !num.includes(value))
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
