e36c21eaef46cc7e12f07d6b789192a2fdf6591b
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-11 21:15:42 -0400

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function (...arr) {
+  let count = 0
+  arr.sort((a, b) => a - b)
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    count += i
+  }
+  if (typeof arr[0] === 'string' || typeof arr[1] === 'string') {
+    return 'ERROR'
+  } else if (arr[0] < 0 || arr[1] < 0) {
+    return 'ERROR'
+  } else if (Array.isArray(arr[0]) || Array.isArray(arr[1])) {
+    return 'ERROR'
+  } else return count
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
