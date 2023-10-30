5f30afd422bf8c49ebb40817fbb3db8ac1df8110
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-30 14:14:33 -0400

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+  var re = /[\W_]/g
+  var lowRegStr = str.toLowerCase().replace(re, '')
+  var reverseStr = lowRegStr.split('').reverse().join('')
+  return reverseStr === lowRegStr
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
