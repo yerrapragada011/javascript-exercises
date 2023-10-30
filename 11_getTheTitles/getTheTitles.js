4485e9ecfc4f17719b430e664e501cd660629df9
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-30 15:24:00 -0400

    Completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (books) {
+  let result = books.map((book) => book.title)
+  return result
+}
 
 // Do not edit below this line
-module.exports = getTheTitles;
+module.exports = getTheTitles
