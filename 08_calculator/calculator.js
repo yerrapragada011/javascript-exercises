483f30ded60c0a5235b843fbddd302729b2932b1
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2023-10-30 13:46:41 -0400

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+  return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+  return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (arr) {
+  return arr.reduce((a, b) => a + b, 0)
+}
 
-const multiply = function() {
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b)
+}
 
-};
+const power = function (a, b) {
+  return Math.pow(a, b)
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function (num) {
+  let result = num
+  if (num === 0 || num === 1) {
+    return 1
+  }
+  while (num > 1) {
+    num--
+    result *= num
+  }
+  return result
+}
 
 // Do not edit below this line
 module.exports = {
@@ -30,4 +38,4 @@ module.exports = {
   multiply,
   power,
   factorial
-};
+}
